2cd19275ca2ffbdd68dda4da03c51845f7c9d9fa
riazul islam <riazulislam5280@gmail.com>
2022-12-03 21:56:19 +0600

    linear search

---
+++ [searching] Linear Search.js
@@ -0,0 +1,12 @@
+const linearSearch = (arr, x) => {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === x) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+let findIndex = linearSearch([1, 4, 5, 9, 2, 6, 3, 7, 8], 90);
+console.log(findIndex);
