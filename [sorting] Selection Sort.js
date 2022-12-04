3d7663145a8141bf921c2f504036b2e041565f40
riazul islam <riazulislam5280@gmail.com>
2022-12-04 07:46:17 +0600

    selection sort

---
+++ [sorting] Selection Sort.js
@@ -0,0 +1,18 @@
+let selectionSort = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+        let minIndex = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        if (minIndex !== i) {
+            arr[i] = arr[minIndex] + (arr[minIndex] = arr[i]) - arr[i];
+        }
+    }
+
+    return arr;
+}
+
+let sortedAra = selectionSort([8, 3, 5, 7, 6, 2, 1, 9, 4, 10]);
+console.log(sortedAra);
---
+++ test.js
@@ -0,0 +1,6 @@
+let a = 10;
+let b = 20;
+
+a = b + (b = a) - a;
+
+console.log(a, b);
